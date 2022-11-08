7ff7a9b6db36d97b905c88ce6a16b558ef200d4e
Ruslan <ruslan.effect@gmail.com>
2022-11-08 18:43:04 +0600

    add something

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("sadasd");
-        System.out.println("sadasd");
+        System.out.println("это строка только для встроенного гита");
+
     }
 }
